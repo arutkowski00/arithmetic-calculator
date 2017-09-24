995bcd5b31c4e4553a7edc5c1181c5135c4fd596
Adam Rutkowski <a_rutkowski@outlook.com>
2017-09-24 20:12:12 +0200

    fix: add missing file & csproj update

---
+++ ArithmeticCalculator/Exceptions/UnsupportedTokenException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ArithmeticCalculator.Tokens;
+
+namespace ArithmeticCalculator.Exceptions
+{
+    public class UnsupportedTokenException : Exception
+    {
+        public UnsupportedTokenException(string parserName, IToken token)
+            : base($"Token type `{token.GetType().Name}` is not supported in {parserName}")
+        {
+        }
+    }
+}
