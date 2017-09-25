462226b2c801b0d9a6b31c76e1f77441f31b1cea
Adam Rutkowski <a_rutkowski@outlook.com>
2017-09-25 23:42:29 +0200

    feat: basic CLI

--- ArithmeticCalculator/Program.cs
+++ ArithmeticCalculator/Program.cs
@@ -1,12 +1,52 @@
-﻿namespace ArithmeticCalculator
+﻿using System;
+using ArithmeticCalculator.Exceptions;
+
+namespace ArithmeticCalculator
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-//            var container = IocContainerSetup.GetContainer();
-//            container.Verify();
-//            var calculator = container.GetInstance<Calculator>();
+            var container = IocContainer.GetContainer();
+            container.Verify();
+
+            var calculator = container.GetInstance<Calculator>();
+
+            const string exitCommand = "exit";
+            
+            while (true)
+            {
+                Console.Write("> ");
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                    continue;
+
+                if (input == exitCommand)
+                    break;
+
+                try
+                {
+                    var result = calculator.Calculate(input);
+                    Console.WriteLine("= {0}", result);
+                }
+                catch (ParseException ex)
+                {
+                    WriteError("[ERROR] {0}", ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    WriteError("[FATAL] {0}", ex.ToString());
+                }
+            }
+        }
+
+        static void WriteError(string format, params object[] args)
+        {
+            var fgColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(format, args);
+            Console.ForegroundColor = fgColor;
         }
     }
 }
