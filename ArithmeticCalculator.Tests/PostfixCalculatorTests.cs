3e8604ca678566dcf3e6db2ea91fe616d2ce9fd8
Adam Rutkowski <a_rutkowski@outlook.com>
2017-09-28 20:00:08 +0200

    test: add more postfix calculator tests

--- ArithmeticCalculator.Tests/PostfixCalculatorTests.cs
+++ ArithmeticCalculator.Tests/PostfixCalculatorTests.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 using ArithmeticCalculator.Algorithms;
+using ArithmeticCalculator.Exceptions;
 using ArithmeticCalculator.Tests.Data;
-using ArithmeticCalculator.Tests.Helpers;
 using ArithmeticCalculator.Tokens;
 using NUnit.Framework;
 
@@ -19,6 +19,36 @@ namespace ArithmeticCalculator.Tests
             var result = _postfixCalculator.Calculate(postfixTokens);
 
             Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void ThrowExceptionIfNumberTokenUsageIsInvalid()
+        {
+            // Equation: 2 2 + 2
+            var tokens = new IToken[]
+            {
+                new NumberToken(2, 1),
+                new NumberToken(2, 3),
+                new OperationToken(OperationType.Add, 5),
+                new NumberToken(2, 7)
+            };
+            
+            Assert.Throws<AggregateException>(() => _postfixCalculator.Calculate(tokens));
+        }
+
+        [Test]
+        public void ThrowExceptionIfOperationTokenUsageIsInvalid()
+        {
+            // Equation: 2 * / 2
+            var tokens = new IToken[]
+            {
+                new NumberToken(2, 1),
+                new OperationToken(OperationType.Multiply, 3),
+                new OperationToken(OperationType.Divide, 5),
+                new NumberToken(2, 7)
+            };
+            
+            Assert.Throws<ParseException>(() => _postfixCalculator.Calculate(tokens));
         }
     }
 }
